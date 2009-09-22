94789a0c5b8323410c94fa3b91bb0281986ede12
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2009-09-23 01:27:07 +0900

    modified spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'activeresource-more'
+require 'rubygems'
+require 'activeresource'
+require 'activeresource/more'
 require 'spec'
 require 'spec/autorun'
 
 Spec::Runner.configure do |config|
-  
+  config.mock_with :rr
 end
