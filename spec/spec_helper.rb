d0ab0e2be3d1d6e9cecc2c21e936b3e04a787601
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2009-09-23 16:37:01 +0900

    fixed required libraries

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
-require 'activeresource'
-require 'activeresource/more'
+require 'active_resource/more'
 require 'spec'
 require 'spec/autorun'
 
